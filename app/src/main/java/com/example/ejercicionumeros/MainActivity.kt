06772b8bf406ef933f7bce8c8f12ca61412c2fd8
Hugo <hugo.reyhol@educa.jcyl.es>
2024-10-03 09:55:24 +0200

    Añadidas alertas y strings a strings.xml

--- app/src/main/java/com/example/ejercicionumeros/MainActivity.kt
+++ app/src/main/java/com/example/ejercicionumeros/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.ejercicionumeros
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.DialogInterface
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,38 +30,55 @@ class MainActivity : AppCompatActivity() {
         val entrada: EditText = findViewById(R.id.entrada)
         val salida: TextView = findViewById(R.id.salida)
 
-        val numMax:Int = 10
-        var numInt:Int = 5
-        var numFin:Int = Random.nextInt(1, numMax+1)
+        val numMax: Int = 10
+        var numInt: Int = 5
+        var numFin: Int = Random.nextInt(1, numMax + 1)
+
+        fun mostrarAlerta(mensaje:Int) {
+            val alerta:AlertDialog.Builder = AlertDialog.Builder(this)
+            alerta.setMessage(getString(mensaje) + getString(R.string.seguir))
+            alerta.setTitle(R.string.tituloDialog)
+
+            alerta.setPositiveButton(R.string.si) { _, _ ->
+                entrada.setText("")
+                salida.setText("")
+
+                numInt = 5
+                numFin = Random.nextInt(1, numMax + 1)
+
+            }
+
+            alerta.setNegativeButton(R.string.no) { _, _ ->
+                finish()
+
+            }
+
+            alerta.create().show()
+
+        }
 
         boton.setOnClickListener {
             try {
-                val num:Int = entrada.text.toString().toInt()
+                val num: Int = entrada.text.toString().toInt()
 
                 if (numInt <= 0) {
-                    salida.text = "Los intentos han llegado a 0, cambiando número"
-
-                    numInt = 5
-                    numFin = Random.nextInt(1, numMax+1)
+                    mostrarAlerta(R.string.fallido)
 
                 } else if (num == numFin) {
-                    salida.text = "Número correcto"
-
-                    numInt = 5
-                    numFin = Random.nextInt(1, numMax+1)
+                    mostrarAlerta(R.string.acertado)
 
                 } else if (num < numFin) {
                     numInt--
-                    salida.text = "El número es mayor, intentos restantes ${numInt}"
+                    salida.text = "${getString(R.string.mayor)} $numInt"
 
                 } else {
                     numInt--
-                    salida.text = "El número es menor, intentos restantes ${numInt}"
+                    salida.text = "${getString(R.string.menor)} $numInt"
 
                 }
 
             } catch (e: Exception) {
-                salida.text = "La entrada debe ser un número entero"
+                salida.setText(R.string.noNumero)
 
             }
         }
